869d54d31c0d495aa27d68ab4e18fa8dcdcedeb1
Nikita-1306 <dhanawadenikita13@gmail.com>
2025-09-23 09:30:17 +0530

    Fix static files with WhiteNoise for Render

--- ccms_project/settings.py
+++ ccms_project/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -24,6 +25,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'ccms_project.urls'
 TEMPLATES = [
     {
